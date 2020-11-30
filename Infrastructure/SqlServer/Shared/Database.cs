728b8331e386125ff26991399a192914abb7fe58
quetslaurent <68534750+quetslaurent@users.noreply.github.com>
2020-11-30 16:31:48 +0100

    AUTO PC DISCOVER FOR CONNECTING TO DB MORE THAN 500000 LINES OF CODING

--- Infrastructure/SqlServer/Shared/Database.cs
+++ Infrastructure/SqlServer/Shared/Database.cs
@@ -5,7 +5,7 @@ namespace Infrastructure.SqlServer.Shared
     public static class Database
     {
         private const string ConnectionString =
-            @"Server=MSI\SQLEXPRESS;Database=sql_fitness_battle;Integrated Security=SSPI";
+            @"Server=.\SQLEXPRESS;Database=sql_fitness_battle;Integrated Security=SSPI";
 
         public static SqlConnection GetConnection()
         {
